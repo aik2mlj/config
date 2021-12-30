ec6f2bd8942608157812e11f6cd4b7d718887932
aik2mlj <aik2mlj@gmail.com>
2021-12-30 12:40:32 +0800

    EDITOR=lvim, add git alias, ruby & miniconda env

--- fish/config.fish
+++ fish/config.fish
@@ -19,7 +19,7 @@ git config --global http.proxy "$PROXY_HTTP"
 git config --global https.proxy "$PROXY_HTTP"
 npm config set proxy="$PROXY_HTTP"
 
-set -gx EDITOR nvim
+set -gx EDITOR lvim
 
 fzf_configure_bindings --directory=\cf
 
@@ -30,7 +30,18 @@ alias ll="exa -l --group-directories-first --icons"
 alias ra="ranger"
 alias vim="nvim"
 
+alias lg="lazygit"
+alias gs="git status"
+alias gl="git log --graph --full-history --all --color --pretty=tformat:\"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s%x20%x1b[33m(%an)%x1b[0m\""
+
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $NPM_PACKAGES/bin $PATH
 set MANPATH $NPM_PACKAGES/share/man $MANPATH
+
+set PATH /home/aik2/.local/share/gem/ruby/3.0.0/bin $PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
 
